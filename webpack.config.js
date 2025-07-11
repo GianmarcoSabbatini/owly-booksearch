98ad81fc088b666b8fc13f92cfaf5795dc434649
GianmarcoSabbatini <gianmarco.sabbatini@yahoo.it>
2025-07-11 11:12:10 +0200

    favicon updated

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist')
+      directory: path.join(__dirname, 'src/img')
     },
     port: 8080,
     open: true
